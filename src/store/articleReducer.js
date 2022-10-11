0f4c5dc6126b66e5283d537d29a9d9acb42fc093
Alexander Feinberg <68302328+alexanderfeinberg@users.noreply.github.com>
2022-10-11 17:14:05 -0400

    Initial commit

---
+++ src/store/articleReducer.js
@@ -0,0 +1,31 @@
+import articles from "../data/data.json";
+
+const LOAD_ARTICLES = "article/loadArticles";
+
+const ADD_ARTICLE = "article/addArticle";
+
+export const loadArticles = () => {
+  return {
+    type: LOAD_ARTICLES,
+    articles
+  };
+};
+
+const initialState = { entries: [], isLoading: true };
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_ARTICLES:
+      return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      return { ...state, entries: [...state.entries, action.article] };
+    default:
+      return state;
+  }
+};
+
+export const addArticle = (article) => {
+  return { type: ADD_ARTICLE, article: article };
+};
+
+export default articleReducer;
